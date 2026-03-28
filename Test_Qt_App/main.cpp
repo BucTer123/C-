9efcf823a42ffeee9e96a06242542ebf46c0beb6
SHALAWA <bambukvova3@gmail.com>
2026-03-28 23:05:35 +0200

    Refactor main window settings and button styles

--- Test_Qt_App/main.cpp
+++ Test_Qt_App/main.cpp
@@ -10,6 +10,9 @@ void btn1_callback(int argc, char *argv[])
 {
     QApplication b(argc, argv);
     QMainWindow w2;
+    w2.resize(640, 480);
+    w2.setWindowTitle("App");
+    
     w2.setObjectName("second_win");
     w2.setStyleSheet(
         "#main_win {"
@@ -52,7 +55,7 @@ void btn1_callback(int argc, char *argv[])
 }
 
 void btn2_callback() {
-    qDebug() 
+    qDebug() << "Shutdown";
     exit(0);
 }
 
@@ -61,41 +64,41 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w;
     w.resize(1920, 1080);
-    w.setTitle("APP");
+    w.setWindowTitle("APP");
     w->setObjectName("main_win");
     w->setStyleSheet(
-        "#main_win {";
-        "   background: white";
-        "   color: black";
-        "   padding: 0";
-        "   margin: 0";
-        "   max-width: 1080px";
-        "   max-height: 1920px";
-        "   width: 100%";
-        "   height: 100%";
-        "}";
+        "#main_win {"
+        "   background: white;"
+        "   color: black;"
+        "   padding: 0;"
+        "   margin: 0;"
+        "   max-width: 1080px;"
+        "   max-height: 1920px;"
+        "   width: 100%;"
+        "   height: 100%;"
+        "}"
     );
 
     QLabel *text1 = new QLabel("Welcome!");
     text1->setObjectName("main_text");
     text1->setStyleSheet(
-        "#main_text {";
-        " background: white";
-        " color: black";
-        " font-family: sans-serif";
-        " font-size: medium";
-        " font-style: initial";
-        " font-weight: bold";
-        "}";
+        "#main_text {"
+        " background: white;"
+        " color: black;"
+        " font-family: sans-serif;"
+        " font-size: medium;"
+        " font-style: initial;"
+        " font-weight: bold;"
+        "}"
     );
 
     QPushButton *btn1 = new QPushButton("New Window");
     btn1->setObjectName("main_btn");
     btn1->setStyleSheet(
-        "#main_btn {";
-        "   background: whitesmoke";
-        "   padding: 10px";
-        "}";
+        "#main_btn {"
+        "   background: whitesmoke;"
+        "   padding: 10px;"
+        "}"
     );
     QObject::connect(btn1, &QPushButton::clicked, btn1_callback);
 
