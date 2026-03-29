263022ea3c66a85d691158200451e9faf802d721
SHALAWA <bambukvova3@gmail.com>
2026-03-29 11:51:26 +0300

    Easy Project

---
+++ Easy_Project/main.cpp
@@ -0,0 +1,30 @@
+#include "mainwindow.h"
+
+#include <QApplication>
+#include <QLabel>
+
+
+QLabel *text1;
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.resize(640, 400);
+    w.setWindowTitle("App");
+
+    text1 = new QLabel("Hui!");
+    text1->setObjectName("txt");
+    text1->setStyleSheet(
+	"#txt {"
+	"color: whitesmoke;"
+	"background: blue;"
+	"}"
+    );
+    text1->setParent(&w);
+    text1->move(10, 20);
+
+
+    w.show();
+    return a.exec();
+}
