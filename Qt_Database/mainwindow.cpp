ac3d76ea5a4e72169cdfb816c27ad1fe0db48060
SHALAWA <bambukvova3@gmail.com>
2026-04-02 14:39:28 +0300

    Qt Database (SQLite)

---
+++ Qt_Database/mainwindow.cpp
@@ -0,0 +1,31 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    db = QSqlDatabase::addDatabase("QSQLITE");
+    db.setDatabaseName("./testDB.db");
+    if (db.open()) {
+        qDebug("open");
+    } else {
+        qDebug("close");
+    }
+
+    query = new QSqlQuery(db);
+    query->exec("CREATE TABLE UZER(First INT,);");
+
+    model = new QSqlTableModel(this, db);
+    model->setTable("UZER");
+    model->select();
+
+    ui->tableView->setModel(model);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
