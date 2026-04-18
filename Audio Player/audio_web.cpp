5e31579951e355fe085b6f0c1003c91533057a1c
SHALAWA <bambukvova3@gmail.com>
2026-04-18 13:01:56 -0400

    Add files via upload

---
+++ Audio Player/audio_web.cpp
@@ -0,0 +1,45 @@
+#include <webview.h>
+#include "audio_web.h"
+
+void audio() {
+    webview::webview w(true, nullptr);
+
+    w.set_title("UI");
+    w.set_size(640, 480, WEBVIEW_HINT_NONE);
+
+    w.set_html(R"(
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8" >
+        <title>UI</title>
+    </head>
+    <style>
+
+    </style>
+    <body>
+        <input id="file" type="file" />
+        <br><br>
+
+        <audio id="pp" accept="video/*"></audio>
+    </body>
+    <script>
+        const inp = document.getElementById("file");
+        const aud = document.getElementById("pp");
+
+        const file = this.files[0];
+        
+        inp.addEventListener("change", function () {
+            if (file) {
+                const url = URL.createObjectURL(file);
+                aud.src = url;
+                aud.play(url);
+            }
+        });
+    </script>
+    </html>    
+    )")
+
+    w.run()
+    return 0;
+}
