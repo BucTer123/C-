37d7b1ff58243df636109d377667ca892671cece
SHALAWA <bambukvova3@gmail.com>
2026-06-12 13:00:17 +0300

    Add files via upload

---
+++ Output(FLTK)/main.cpp
@@ -0,0 +1,14 @@
+#include <FL/Fl.H>
+#include <FL/Fl_Window.H>
+#include <FL/Fl_Output.H>
+
+int main() {
+    Fl_Window *win = new Fl_Window(640, 480, "Window!");
+    win->show();
+
+    Fl_Output *txt = new Fl_Output(10, 20, 30, 40, "Welcome!");
+    txt->show();
+
+    Fl::run();
+    exit(0);
+}
