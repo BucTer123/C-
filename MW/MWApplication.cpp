8b4b082157eb514d14b9e84aaf8e57ea012eae13
SHALAWA <bambukvova3@gmail.com>
2026-07-14 19:37:24 +0300

    Update MWApplication.cpp

--- MW/MWApplication.cpp
+++ MW/MWApplication.cpp
@@ -1,7 +1,6 @@
 #include "uidir/UI.h"
 #include "MWApplication.h"
 #include "cmddir/cmd.h"
-#include "uidir/OUI.h"
 #include <iostream>
 #include <string>
 
@@ -46,8 +45,5 @@ class MVApp : public MVApplication {
        }
        void cmd_main_text() {
            CMD.main_text();
-       }
-       void oldui_create_window(int screen_widthold_from_function, int screen_heightold_from_function, std::string name_windowold) {
-            OUT.create_new_window();
        }
 };
