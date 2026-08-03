2459474337cce7f8df5bc49a9a9257cc7921ca31
SHALAWA <bambukvova3@gmail.com>
2026-08-03 17:04:17 +0200

    Add files via upload

---
+++ Test_Rasberypico/main.cpp
@@ -0,0 +1,27 @@
+#include "pico/stdlib.h"
+#include "hardware/gpio.h"
+#include "hardware/uart.h"
+
+int main() {
+	stdio_init_all();
+
+	while (!stdio_usb_connected()) {
+		sleep_ms(1000);
+	}
+
+	printf("Hello, world!\n");
+
+	uart_init(uart0, 115200);
+	
+	gpio_set_function(0, GPIO_FUNCTION);
+	gpio_set_function(1, GPIO_FUNCTION);
+
+	uart_puts(uart0, "Starting ...");
+
+	while (true) {
+		for (int i = 0; i < 1000; i++) {
+			printf("%d\n", i);
+			sleep_ms(1000);
+		}	
+	}
+}
