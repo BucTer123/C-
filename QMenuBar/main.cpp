7f016aea3d516d019fc2fb305da290450426becb
Boba <bambukvova3@gmail.com>
2026-08-15 13:50:32 +0200

    Update main.cpp

--- QMenuBar/main.cpp
+++ QMenuBar/main.cpp
@@ -14,26 +14,23 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w;
     w.setWindowTitle("MENU");
+    w.resize(800, 600);
 
     QLabel *text = new QLabel("YA PIDORAZ!", &w);
-    text->move(10, 20);
+    text->move(400, 10);
 
 
     QMenuBar *men = new QMenuBar(&w);
     QMenu *file = new QMenu(&w);
     file->addMenu("&File");
+    file->addMenu("Info");
+    men->addMenu(file);
     men->addMenu(file);
 
     QSlider *s1 = new QSlider(Qt::Horizontal, &w);
     QSlider *s2 = new QSlider(Qt::Vertical, &w);
     QSpinBox *sb = new QSpinBox;
     sb->setParent(&w);
-
-    QHBoxLayout *L = new QHBoxLayout;
-    L->addWidget(s1);
-    L->addWidget(s2);
-    L->addWidget(sb);
-
 
     w.show();
     return a.exec();
